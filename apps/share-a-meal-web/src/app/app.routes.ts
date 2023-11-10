b8bfe65f659a65ea2bca4b4b05bd2830bae65d32
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-11-10 11:38:25 +0100

    fix (navigeren nav-bar) routermodule importeren in uimodule

--- apps/share-a-meal-web/src/app/app.routes.ts
+++ apps/share-a-meal-web/src/app/app.routes.ts
@@ -6,10 +6,10 @@ export const appRoutes: Route[] = [
         path:'about', 
         pathMatch:'full',
         component:AboutComponent
-    }
-    //  {
-    //     path:'features',
-    //     loadChildren:()=> import ('@avans-nx-workshop/share-a-meal/features').then((esModule)=> esModule.FeaturesModule)
+    },
+     {
+        path:'features',
+        loadChildren:()=> import ('@avans-nx-workshop/share-a-meal/features').then((esModule)=> esModule.FeaturesModule)
 
-    // }
+    }
 ];
--- libs/share-a-meal/ui/src/lib/ui.module.ts
+++ libs/share-a-meal/ui/src/lib/ui.module.ts
@@ -1,10 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent,FooterComponent],
 })
